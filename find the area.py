8d30afdbb0c2323ba56ab68f1288aa828ae6caf7
Rijo P <37271845+rijopp@users.noreply.github.com>
2023-02-15 21:13:38 -0500

    Create find the area.py

---
+++ find the area.py
@@ -0,0 +1,14 @@
+# Python program to find the area of a triangle
+
+# ask user for the length of the base and the height of the triangle
+base = float(input("Enter the length of the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+# calculate the area of the triangle using the formula: (base * height) / 2
+area = (base * height) / 2
+
+# print the area of the triangle
+print("The area of the triangle is:", area)
+# in this program, we first ask the user to enter the length of the base and the height of the triangle.
+#  We then calculate the area of the triangle using the formula: (base * height) / 2. Finally,
+# we print the area of the triangle using the print function.
